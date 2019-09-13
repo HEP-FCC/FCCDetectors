vvolkl/FCCSW

[FCCeeLAr] add options files for sampling fraction jobs

Detector/DetStudies/tests/options/fcc_ee_upstreamMaterial_inclinedEcal.py
@@ -1,0 +1,78 @@
+from Gaudi.Configuration import *
+
+# Data service
+from Configurables import FCCDataSvc
+podioevent = FCCDataSvc("EventDataSvc")
+
+# DD4hep geometry service
+from Configurables import GeoSvc
+geoservice = GeoSvc("GeoSvc", detectors=[ 'file:Detector/DetFCCeeIDEA-LAr/compact/FCChh_DectMaster.xml',
+],
+                    OutputLevel = WARNING)
+
+# Geant4 service
+# Configures the Geant simulation: geometry, physics list and user actions
+from Configurables import SimG4Svc
+geantservice = SimG4Svc("SimG4Svc", detector='SimG4DD4hepDetector', physicslist="SimG4FtfpBert", actions="SimG4FullSimActions")
+geantservice.g4PostInitCommands += ["/run/setCut 0.1 mm"]
+
+# Geant4 algorithm
+# Translates EDM to G4Event, passes the event to G4, writes out outputs via tools
+# and a tool that saves the calorimeter hits
+from Configurables import SimG4Alg, SimG4SaveCalHits, SimG4SingleParticleGeneratorTool
+saveecaltool = SimG4SaveCalHits("saveECalBarrelHits",readoutNames = ["ECalBarrelEta"])
+saveecaltool.positionedCaloHits.Path = "ECalBarrelPositionedHits"
+saveecaltool.caloHits.Path = "ECalBarrelHits"
+from Configurables import SimG4SingleParticleGeneratorTool
+pgun=SimG4SingleParticleGeneratorTool("SimG4SingleParticleGeneratorTool",saveEdm=True,
+                                      particleName = "e-", energyMin = 50000, energyMax = 50000, etaMin = 0, etaMax = 0, phiMin = -0.1, phiMax = 0.1,
+                                      OutputLevel = DEBUG)
+# next, create the G4 algorithm, giving the list of names of tools ("XX/YY")
+geantsim = SimG4Alg("SimG4Alg",
+                    outputs= ["SimG4SaveCalHits/saveECalBarrelHits"],
+                    eventProvider = pgun,
+                    OutputLevel = DEBUG)
+
+
+from Configurables import CreateCaloCells
+createcellsBarrel = CreateCaloCells("CreateCaloCellsBarrel",
+                                    doCellCalibration=False,
+                                    addCellNoise=False, filterCellNoise=False)
+createcellsBarrel.hits.Path="ECalBarrelHits"
+createcellsBarrel.cells.Path="ECalBarrelCells"
+
+from Configurables import UpstreamMaterial
+hist = UpstreamMaterial("histsPresampler",
+                        energyAxis=50,
+                        phiAxis=0.1,
+                        readoutName = "ECalBarrelEta",
+                        layerFieldName = "layer",
+                        numLayers = 8,
+                        # sampling fraction is given as the upstream correction will be applied on calibrated cells
+                        samplingFraction = [0.12125] + [0.14283] + [0.16354] + [0.17662] + [0.18867] + [0.19890] + [0.20637] + [0.20802],
+                        OutputLevel = VERBOSE)
+hist.deposits.Path="ECalBarrelCells"
+hist.particle.Path="GenParticles"
+
+THistSvc().Output = ["det DATAFILE='histUpstream_fccee_hits_e50GeV_eta0_Bfield1_10events_8layers.root' TYP='ROOT' OPT='RECREATE'"]
+THistSvc().PrintAll=True
+THistSvc().AutoSave=True
+THistSvc().AutoFlush=True
+THistSvc().OutputLevel=INFO
+
+#CPU information
+from Configurables import AuditorSvc, ChronoAuditor
+chra = ChronoAuditor()
+audsvc = AuditorSvc()
+audsvc.Auditors = [chra]
+geantsim.AuditExecute = True
+hist.AuditExecute = True
+
+# ApplicationMgr
+from Configurables import ApplicationMgr
+ApplicationMgr( TopAlg = [geantsim, createcellsBarrel, hist],
+                EvtSel = 'NONE',
+                EvtMax = 10,
+                # order is important, as GeoSvc is needed by G4SimSvc
+                ExtSvc = [podioevent, geoservice, geantservice, audsvc],
+                OutputLevel = DEBUG)

HEP-FCC/FCCDetectors

DetStudies/tests/options/fcc_ee_upstreamMaterial_inclinedEcal.py
@@ -1,0 +1,83 @@
+from Gaudi.Configuration import *
+
+# Data service
+from Configurables import FCCDataSvc
+podioevent = FCCDataSvc("EventDataSvc")
+
+# DD4hep geometry service
+from Configurables import GeoSvc
+geoservice = GeoSvc("GeoSvc", detectors=[ 'file:Detector/DetFCCeeIDEA-LAr/compact/FCCee_DectMaster.xml',
+],
+                    OutputLevel = WARNING)
+
+# Geant4 service
+# Configures the Geant simulation: geometry, physics list and user actions
+from Configurables import SimG4Svc
+geantservice = SimG4Svc("SimG4Svc", detector='SimG4DD4hepDetector', physicslist="SimG4FtfpBert", actions="SimG4FullSimActions")
+geantservice.g4PostInitCommands += ["/run/setCut 0.1 mm"]
+
+# Geant4 algorithm
+# Translates EDM to G4Event, passes the event to G4, writes out outputs via tools
+# and a tool that saves the calorimeter hits
+from Configurables import SimG4Alg, SimG4SaveCalHits, SimG4SingleParticleGeneratorTool
+saveecaltool = SimG4SaveCalHits("saveECalBarrelHits",readoutNames = ["ECalBarrelEta"])
+saveecaltool.positionedCaloHits.Path = "ECalBarrelPositionedHits"
+saveecaltool.caloHits.Path = "ECalBarrelHits"
+from Configurables import SimG4SingleParticleGeneratorTool
+pgun=SimG4SingleParticleGeneratorTool("SimG4SingleParticleGeneratorTool",saveEdm=True,
+                                      particleName = "e-", energyMin = 50000, energyMax = 50000, etaMin = 0, etaMax = 0, phiMin = -0.1, phiMax = 0.1,
+                                      OutputLevel = DEBUG)
+# next, create the G4 algorithm, giving the list of names of tools ("XX/YY")
+geantsim = SimG4Alg("SimG4Alg",
+                    outputs= ["SimG4SaveCalHits/saveECalBarrelHits"],
+                    eventProvider = pgun,
+                    OutputLevel = DEBUG)
+
+
+from Configurables import CreateCaloCells
+createcellsBarrel = CreateCaloCells("CreateCaloCellsBarrel",
+                                    doCellCalibration=False,
+                                    addCellNoise=False, filterCellNoise=False)
+createcellsBarrel.hits.Path="ECalBarrelHits"
+createcellsBarrel.cells.Path="ECalBarrelCells"
+
+from Configurables import UpstreamMaterial
+hist = UpstreamMaterial("histsPresampler",
+                        energyAxis=50,
+                        phiAxis=0.1,
+                        readoutName = "ECalBarrelEta",
+                        layerFieldName = "layer",
+                        numLayers = 8,
+                        # sampling fraction is given as the upstream correction will be applied on calibrated cells
+                        samplingFraction = [0.12125] + [0.14283] + [0.16354] + [0.17662] + [0.18867] + [0.19890] + [0.20637] + [0.20802],
+                        OutputLevel = VERBOSE)
+hist.deposits.Path="ECalBarrelCells"
+hist.particle.Path="GenParticles"
+
+THistSvc().Output = ["det DATAFILE='histUpstream_fccee_hits_e50GeV_eta0_Bfield1_8layers.root' TYP='ROOT' OPT='RECREATE'"]
+THistSvc().PrintAll=True
+THistSvc().AutoSave=True
+THistSvc().AutoFlush=True
+THistSvc().OutputLevel=INFO
+
+#CPU information
+from Configurables import AuditorSvc, ChronoAuditor
+chra = ChronoAuditor()
+audsvc = AuditorSvc()
+audsvc.Auditors = [chra]
+geantsim.AuditExecute = True
+hist.AuditExecute = True
+
+from Configurables import PodioOutput
+### PODIO algorithm
+out = PodioOutput("out",OutputLevel=DEBUG)
+out.outputCommands = ["keep *"]
+
+# ApplicationMgr
+from Configurables import ApplicationMgr
+ApplicationMgr( TopAlg = [geantsim, createcellsBarrel, hist, out],
+                EvtSel = 'NONE',
+                EvtMax = 10,
+                # order is important, as GeoSvc is needed by G4SimSvc
+                ExtSvc = [podioevent, geoservice, geantservice, audsvc],
+                OutputLevel = DEBUG)