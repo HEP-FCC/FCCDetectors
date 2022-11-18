ab2d7939c0de1efd65740ade247ab8849551dbdc
Lorenzo Capriotti <lorenzo.capriotti@cern.ch>
2022-11-18 17:22:32 +0100

    removed UpgradeTags.h from list of includes

--- Detector/DetFCCeeIDEA/src/DriftChamber.cpp
+++ Detector/DetFCCeeIDEA/src/DriftChamber.cpp
@@ -8,7 +8,6 @@
 * granted to it by virtue of its status as an Intergovernmental Organization  *
 * or submit itself to any jurisdiction.                                       *
 \*****************************************************************************/
-#include "Core/UpgradeTags.h"
 #include "DD4hep/DetFactoryHelper.h"
 #include "DD4hep/Printout.h"
 #include "DD4hep/detail/DetectorInterna.h"
